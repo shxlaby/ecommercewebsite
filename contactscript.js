eccbb24867a72edcd1ab5abe6444864bda238e30
Ahmed <eid09772@gmail.com>
2024-12-21 19:24:48 +0200

    add Get From Fileds

--- contactscript.js
+++ contactscript.js
@@ -1,2 +1,7 @@
 document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent form submission
+
+    // Get form fields
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
